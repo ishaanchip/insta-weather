071b78fc28907a26362818c1f124a4cbda9b864a
ishaanchip <ishaanchiplunkar@gmail.com>
2024-12-01 22:52:53 -0500

    hopefully making refresh work

--- src/Router.js
+++ src/Router.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {Route, Routes, BrowserRouter} from 'react-router-dom'
 import Home from './components/Home/Home'
 import Catalog from './components/Catalog/Catalog'
-import HandleRefresh from './components/Refresh/HandleRefresh'
-
 
 const Router = () => {
+  useEffect(() => {
+    const handleRefresh = () => {
+      window.location.href = '/'
+    }
+    
+    window.addEventListener('beforeunload', handleRefresh)
+    
+    return () => {
+      window.removeEventListener('beforeunload', handleRefresh)
+    }
+  }, [])
+
   return (
     <BrowserRouter>
-        <HandleRefresh />
         <Routes>
             <Route path="/" element={<Home/>}></Route>
             <Route path="/weather-finder" element={<Catalog/>}></Route>
