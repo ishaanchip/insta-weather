b9964bbc552f200b3762966666442b7a9a135618
ishaanchip <ishaanchiplunkar@gmail.com>
2024-12-02 18:49:06 -0500

    pls be last routing switch

--- src/Router.js
+++ src/Router.js
@@ -1,21 +1,9 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import {Route, Routes, BrowserRouter} from 'react-router-dom'
 import Home from './components/Home/Home'
 import Catalog from './components/Catalog/Catalog'
 
 const Router = () => {
-  useEffect(() => {
-    const handleRefresh = () => {
-      window.location.href = '/'
-    }
-    
-    window.addEventListener('beforeunload', handleRefresh)
-    
-    return () => {
-      window.removeEventListener('beforeunload', handleRefresh)
-    }
-  }, [])
-
   return (
     <BrowserRouter>
         <Routes>
